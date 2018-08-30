cd5a93eb9bba478e562eb7438608604400dc0d34
Antti Partanen <aehparta@iki.fi>
2018-08-30 14:42:42 +0300

    check that doctrine entity manager is ok

--- config/cli-config.php
+++ config/cli-config.php
@@ -5,21 +5,16 @@
  */
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
-/* create kernel */
 $kernel = kernel::getInstance();
 $kernel->load(__DIR__);
 $em = get_entity_manager();
-
-if (method_exists('Doctrine\ORM\Tools\Console\ConsoleRunner', 'createHelperSet'))
-{
+if (!$em) {
+    throw new Exception('doctrine not configured, cannot fetch entity manager');
+} else if (method_exists('Doctrine\ORM\Tools\Console\ConsoleRunner', 'createHelperSet')) {
     return Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($em);
-}
-else
-{
+} else {
     $helperSet = new \Symfony\Component\Console\Helper\HelperSet(array(
         'db' => new \Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper($em->getConnection()),
         'em' => new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($em),
     ));
 }
-
